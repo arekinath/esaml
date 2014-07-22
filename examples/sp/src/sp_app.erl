e2279ccf7f8b10eab2f73a9bfd31d7947cd9df6a
Alex Wilson <alex@uq.edu.au>
2014-07-22 08:52:49 +0000

    Add SP example code

---
+++ examples/sp/src/sp_app.erl
@@ -0,0 +1,28 @@
+%% esaml - SAML for erlang
+%%
+%% Copyright (c) 2013, Alex Wilson and the University of Queensland
+%% All rights reserved.
+%%
+%% Distributed subject to the terms of the 2-clause BSD license, see
+%% the LICENSE file in the root of the distribution.
+
+-module(sp_app).
+
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/saml/:operation", sp_handler, []}
+		]}
+	]),
+	{ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
+		{env, [{dispatch, Dispatch}]}
+	]),
+	sp_sup:start_link().
+
+stop(_State) ->
+	ok.
---
+++ examples/sp/src/sp_sup.erl
@@ -0,0 +1,19 @@
+%% esaml - SAML for erlang
+%%
+%% Copyright (c) 2013, Alex Wilson and the University of Queensland
+%% All rights reserved.
+%%
+%% Distributed subject to the terms of the 2-clause BSD license, see
+%% the LICENSE file in the root of the distribution.
+
+-module(sp_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, init/1]).
+
+start_link() ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+	Procs = [],
+	{ok, {{one_for_one, 10, 10}, Procs}}.
