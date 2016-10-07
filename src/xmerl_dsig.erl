233c8076783f5de8d5240b92aec26f59f97de966
Jaap Frolich <jfrolich@gmail.com>
2016-10-07 17:01:09 +0200

    make string

--- src/xmerl_dsig.erl
+++ src/xmerl_dsig.erl
@@ -65,7 +65,7 @@ sign(ElementIn, PrivateKey = #'RSAPrivateKey'{}, CertBin, SigMethod) when is_bin
             case lists:keyfind('id', 2, ElementStrip#xmlElement.attributes) of
                 #xmlAttribute{value = LowId} -> {ElementStrip, LowId};
                 _ ->
-                    NewId = 'a' ++ uuid:to_string(uuid:uuid1()),
+                    NewId = "a" ++ uuid:to_string(uuid:uuid1()),
                     Attr = #xmlAttribute{name = 'ID', value = NewId, namespace = #xmlNamespace{}},
                     NewAttrs = [Attr | ElementStrip#xmlElement.attributes],
                     Elem = ElementStrip#xmlElement{attributes = NewAttrs},
